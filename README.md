5bc51d85654cd69778a7cb17452122f44f876bb8
Jeff Inman <jeffery.inman@hotmail.com>
2021-01-15 22:08:02 -0800

    created files - populated html and js files - needs debugging

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # color-game
 Youtube walkthrough from link
 https://youtu.be/9miwgZmiXkQ
+
+Problem statement
+- create a color game
+  - pick right color based on RGB ratio
+  - use CSS to style the grids adn JS fuctions to recolor grids
+
+- Requirements
+  - VS studio
+
+- Output
+  - Color game with colored squares
+
+- intention of follow along
+  - to get more exposure to different ways of using HTML, CSS, and JS functions - using the skills i have learned so far to fill in the blanks of the youtube tutorials
---
+++ app.js
@@ -0,0 +1,95 @@
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
+var square = document.querySelectorAll('square');
+var pickedColor = pickColor();
+var rgbCode = document.getElementById('rgbCode');
+var messageDisplay = document.querySelector('#message');
+var h1 = docuemnt.querySelector('h1');
+var resetButton = document.querySelector('#reset');
+var easyButton = document.quesrySelector('#easyButton');
+var hardButton = document.querySelector('#hardButton');
+
+easyButton.addEventListener('click', function() {
+  hardButton.classList.remove('selected');
+  easyButton.classList.add('selected');
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  rgbCode.textContent = pickedColor;
+  for(var i = 0; i < square.length; i++) {
+    if (colors[i]) {
+      square[i].getElementsByClassName.background = colors[i];
+    } else {
+      square[i].getElementsByClassName.display = 'none';
+    }
+  }
+});
+
+hardButton.addEventListener('click', function() {
+  easyButton.classList.remove('selected');
+  hardButton.classList.add('selected');
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  rgbCode.textContent = pickedColor;
+  for(var i = 0; i < square.length; i++) {
+    square[i].getElementsByClassName.background = colors[i];
+    square[i].getElementsByClassName.display = 'block';
+  }
+});
+
+resetButton.addEventListener('click', function() {
+  colors = generateRandomColors(numsquares);
+  pickedColor = pickColor();
+  rgbCode.textContent = pickedColor;
+  this.textContent = 'New Color';
+  for (var i = 0; i < square.length; i++) {
+    square[i].getElementsByClassName.background = colors[i];
+  }
+  h1.style.background = 'steelblue';
+})
+
+rgbCode.textContent = pickedColor;
+
+for (var = 0; i < square.length; i++) {
+  square[i].style.background = colors[i];
+  square[i].addEventListner('click', function() {
+    var clickedColor = this.style.background;
+    if (clickedColor === pickedColor) {
+      messageDisplay.textContent = 'Good Job!';
+      resetButton.textContent = 'Play Agaiin?';
+      changeColors(clickedColor);
+      h1.style.background = clickedColor;
+    } else {
+      this.style.background = '#232323';
+      messageDisplay.textContent = 'Try Again';
+    }
+  })
+}
+
+function changeColors(color) {
+  for (var i = 0; i < square.length; i++) {
+    square[i].style.background = color;
+  }
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length)
+  return colors[random];
+}
+
+function generateRandomColors(num) {
+  var arr = []
+  for (var i = 0; i < num; i++) {
+    arr.push(randomColor())
+  }
+  return arr;
+}
+
+function randomColor() {
+  var r = Math.floor(Math.random()) * 256);
+  var g = Math.floor(Math.random()) * 256);
+  var b = Math.floor(Math.random()) * 256);
+
+  return 'rgb(' + r+ ', ' + g + ', ' + b + ')';
+  }
